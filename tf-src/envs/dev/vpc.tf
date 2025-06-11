a6afd159fc98a0935822f77a9f2ae345a4837461
samishken <samishken@gmail.com>
2025-06-11 13:21:28 -0400

    upd s3 module

--- tf-src/envs/dev/vpc.tf
+++ tf-src/envs/dev/vpc.tf
@@ -15,3 +15,17 @@ module "audit-report-app-vpc" {
     Environment = "${local.environment}"
   }
 }
+
+module "audit-report-app-bucket" {
+  source = "git::https://github.com/samishken/audit-report-app.git//tf-src/modules/s3"
+  # source = "../../modules/s3"
+  bucket_name         = "${local.environment}-${local.project_name}-bucket"
+  versioning  = true
+
+  tags = {
+    Terraform   = "true"
+    Name        = "${local.environment}-${local.project_name}-bucket"
+    Project     = "${local.project_name}"
+    Environment = "${local.environment}"
+  }
+}
