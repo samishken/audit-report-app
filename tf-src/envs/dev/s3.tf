787314241a95ea69b8762c9252290572e5d33de0
samishken <samishken@gmail.com>
2025-06-11 14:17:48 -0400

    deploy s3 buckets

--- tf-src/envs/dev/s3.tf
+++ tf-src/envs/dev/s3.tf
@@ -4,10 +4,10 @@ resource "random_id" "bucket_suffix" {
 }
 
 module "audit-report-app-bucket" {
-  # source = "git::https://github.com/samishken/audit-report-app.git//tf-src/modules/s3"
-  source = "../../modules/s3"
-  count = 2
-  bucket_name         = "${local.environment}-${local.project_name}-${random_id.bucket_suffix[count.index].hex}"
+  source = "git::https://github.com/samishken/audit-report-app.git//tf-src/modules/s3"
+  # source      = "../../modules/s3"
+  count       = 2
+  bucket_name = "${local.environment}-${local.project_name}-${random_id.bucket_suffix[count.index].hex}"
   versioning  = true
 
   tags = {
