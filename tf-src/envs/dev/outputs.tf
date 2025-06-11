19c0f8b67b9d384ee28cec7f9cafd7f6da49bd8d
samishken <samishken@gmail.com>
2025-06-11 12:44:34 -0400

    add vpc

---
+++ tf-src/envs/dev/outputs.tf
@@ -0,0 +1,8 @@
+output "instance_ip_address" {
+  description = "The public IP address of the instance"
+  value       = values(aws_instance.my_server)[*].public_ip
+}
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = module.audit-report-app-vpc.vpc_id
+}
