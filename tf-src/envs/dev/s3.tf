1fe679ff101ef310928342757704b4123076778c
samishken <samishken@gmail.com>
2025-06-11 14:19:53 -0400

    s3

--- tf-src/envs/dev/s3.tf
+++ tf-src/envs/dev/s3.tf
@@ -4,8 +4,8 @@ resource "random_id" "bucket_suffix" {
 }
 
 module "audit-report-app-bucket" {
-  source = "git::https://github.com/samishken/audit-report-app.git//tf-src/modules/s3"
-  # source      = "../../modules/s3"
+  # source = "git::https://github.com/samishken/audit-report-app.git//tf-src/modules/s3"
+  source      = "../../modules/s3"
   count       = 2
   bucket_name = "${local.environment}-${local.project_name}-${random_id.bucket_suffix[count.index].hex}"
   versioning  = true
